69eded31087725addb59399eb9fb93b7bf0cf542
Ioannis Karalis <i.karalis@outlook.de>
2025-10-15 22:28:24 +0200

    classes & character added

---
+++ models/character.class.js
@@ -0,0 +1,10 @@
+class Character extends MovableObject{
+
+    constructor(){
+        super().loadImage('img/2.Sharkie/1.IDLE/1.png')
+    }
+
+    jump(){
+
+    }
+}
---
+++ models/fish.class.js
@@ -0,0 +1,8 @@
+class Fish extends MovableObject {
+
+
+    constructor(){
+        super().loadImage('img/3.Enemy/1.Puffer fish/1.Swim/3.swim1.png')
+    }
+
+}
