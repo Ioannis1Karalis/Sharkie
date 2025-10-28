4ccc41bc915ef354ff397b213f744e03bec8efcf
Ioannis Karalis <i.karalis@outlook.de>
2025-10-28 22:24:12 +0100

    background frame

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -22,6 +22,21 @@ class MovableObject {
         });
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+
+        if(this instanceof Character || this instanceof Fish){
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'red';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+        }
+    }
+
     playAnimation(images) {
         let i = this.currentImage % this.IMAGES_SWIMMING.length; // let i = 0 % 6; 0, => Rest 0 // i = 0, 1, 2, 3, 4, 5, 6,... 0, 1, 2  
         let path = images[i];
@@ -37,6 +52,5 @@ class MovableObject {
         setInterval(() => {
             this.x -= this.speed;
         }, 1000 / 60);
-
     }
 }
