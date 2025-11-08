ddb5a4d5dc83a2e0f57dfa219502b74abe8fb47b
Ioannis Karalis <i.karalis@outlook.de>
2025-11-08 17:00:36 +0100

    background img & font added

---
+++ models/poison.class.js
@@ -0,0 +1,10 @@
+class Poison extends DrawbleObject {
+
+    IMAGE_POISON_LEFT = [];
+
+    IMAGE_POISON_RIGHT = [];
+
+    constructor() {
+        super().loadImage(this.IMAGE_POISON_LEFT);
+    }
+}
